be92a5262ca609ca39fcb3f1716728b63a7fbbe0
YUKIJUDAI <1787149082@qq.com>
2020-12-10 15:55:34 +0800

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: "http://mars-api.huoxingqingbao.com",
+    baseUrl: "",
     proxyid: 0
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    baseUrl: "http://mars-api.huoxingqingbao.com",
+    baseUrl: "",
     proxyid: 0
 };
 
